f56455392cab338aad6d7462942c836f0f07e965
tea.liao <tea.liao@bytedance.com>
2018-03-12 13:48:19 +0800

    save

--- Documents/WORKSPACE/partice/index.js
+++
@@ -1,6 +0,0 @@
-var c = document.getElementById("canvas");
-var canvasContext = c.getContext("2d");
-var img = new Image()
-img.onload = () => canvasContext.drawImage(img,10,10)
-img.src = './assets/m.svg'
-
