686f298bf6a2191b6d86519be17fda24c5619797
Liaohuanle <Liaohuanle@users.noreply.github.com>
2018-06-17 17:17:19 +0800

    Update sort_math.js

--- sort_math.js
+++ sort_math.js
@@ -1,23 +1,19 @@
 /*
 * 快速排序
 */
-
 function sortQ(list){
-  if(!list || list.length <= 0 ){
+  if( !Array.isArray(list) || !list || list.length <= 0 ){
     return []
   }
   if(list.length <= 1){
     return list
   }
-  const right = []
-  const left = []
-  const baseN = list[0]
-  const sortList = list.slice(1)
-  sortList.forEach((i, index) => {
-    if(baseN >= sortList[index]){
-      right.push(sortList[index])
+  const right = [], left = [], baseN = list[0], sortList = list.slice(1)
+  sortList.forEach((i) => {
+    if(baseN >=i){
+      right.push(i)
     }else{
-      left.push(sortList[index])
+      left.push(i)
     }
   })
   return [].concat(sortQ(right),[baseN],sortQ(left))
